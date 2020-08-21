2e0e6c06ff11b782aa0aa9a9a9386ef8d0047266
ovchynnikov <vapore18@gmail.com>
2020-08-21 13:10:02 +0300

    Input component, test validation

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import classes from './Auth.css';
-import Button from '../../components/UI/Button/Button'
+import Button from '../../components/UI/Button/Button';
+import Input from '../../components/UI/Input/Input';
 
 export default class Auth extends Component {
 
@@ -24,8 +25,12 @@ export default class Auth extends Component {
                    <h1>Auth page</h1>
 
                    <form onSubmit={this.submitHandler} className={classes.AuthForm}>
-                       <input type="text"/>
-                       <input type="text"/>
+                       <Input label="Email"
+                              
+                       />
+                       <Input label="Password" 
+                              errorMessage={'Test'}  
+                       />
 
                        <Button 
                        type='success' 
