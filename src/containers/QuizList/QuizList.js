d50ea903e4bb4ca7d42955853a3297466d586950
ovchynnikov <vapore18@gmail.com>
2020-08-25 12:27:59 +0300

    Creation of quiz to backend Firebase DB

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -1,19 +1,46 @@
 import React, {Component} from 'react';
 import classes from './QuizList.css';
 import { NavLink } from 'react-router-dom';
+import axios from 'axios';
 
 export default class QuizList extends Component {
 
+    state = {
+      quizes: [],
+
+    }
+
+
     renderQuizes() {
-        return [1,2,3].map((quiz, index) => {
+        return this.state.quizes.map(quiz => {
              return (
-                 <li key={index}>
-                     <NavLink to={'/quiz/' + quiz}> Quiz number {quiz}</NavLink>
+                 <li key={quiz.id}>
+                     <NavLink to={'/quiz/' + quiz.id}> {quiz.name}</NavLink>
                  </li>
              )
         })
     }
 
+    /* request to the backend server should be when we already have DOM tree  */
+    async componentDidMount(){
+      try{
+       const response = await axios.get('https://react-quiz-31d3a.firebaseio.com/quizes.json')
+       const quizes = []
+
+        Object.keys(response.data).forEach((key, index) => {
+          quizes.push({
+            id: key,
+            name: `Quiz № ${index + 1}`
+          })
+        })
+        this.setState({
+          quizes
+        })
+
+      } catch(e) {
+        console.log(e)
+      }
+    }
 
     render() {
         return (
